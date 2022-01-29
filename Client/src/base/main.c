707efc7dadd728acdb86c2a421713bb54cc4ff56
NIka-netizen <58162327+NIka-netizen@users.noreply.github.com>
2022-01-29 12:41:58 +0200

    Add files via upload

--- Client/src/base/main.c
+++ Client/src/base/main.c
@@ -1,19 +1,22 @@
 #include "client.h"
 
-void open_chat (GtkApplication *app, gpointer user_data) {
-    GtkWidget *window1;
-    window1 = gtk_application_window_new (app);
-    gtk_window_set_title (GTK_WINDOW (window1), "Window");
-    gtk_window_set_default_size (GTK_WINDOW (window1), 200, 200);
-    gtk_widget_show_all (window1);
+void change_password (GtkWidget *window, GdkEvent *event, gpointer user_data) {
+  gtk_widget_destroy(gtk_widget_get_parent(window));
+    /*  ВОТ ТУТ ОКНО СО СМЕНОЙ ПАРОЛЯ */
 }
 
-void reg_window (GtkApplication *app, gpointer user_data) {
-    GtkWidget *window1;
-    window1 = gtk_application_window_new (app);
-    gtk_window_set_title (GTK_WINDOW (window1), "Window");
-    gtk_window_set_default_size (GTK_WINDOW (window1), 200, 200);
-    gtk_widget_show_all (window1);
+void open_chat (GtkWidget *window, GdkEvent *event, gpointer user_data) 
+{ 
+    gtk_widget_destroy(gtk_widget_get_parent(window));
+    /*  ВОТ ТУТ ОКНО С ЧАТОМ */
+}
+
+void open_reg (GtkWidget *window, GdkEvent *event, gpointer user_data) 
+{ 
+
+    gtk_widget_destroy(gtk_widget_get_parent(window));
+    /*  ВОТ ТУТ ОКНО С РЕГИСТРАЦИЕЙ */
+    
 }
 
 static void activate (GtkApplication *app, gpointer user_data)
@@ -21,26 +24,70 @@ static void activate (GtkApplication *app, gpointer user_data)
   GtkWidget *window;
   GtkWidget *button_log;
   GtkWidget *button_reg;
+  GtkWidget *forg_password;
   GtkWidget *button_box;
+  GtkWidget *label_edit_box;
+  GtkWidget *label[2];
+  GtkWidget *edit[2];
 
+
+  //window
   window = gtk_application_window_new (app);
   gtk_window_set_title (GTK_WINDOW (window), "Registration");
-  gtk_window_set_default_size (GTK_WINDOW (window), 400, 400);
+  gtk_container_set_border_width (GTK_CONTAINER (window), 10);
+  gtk_window_set_default_size (GTK_WINDOW (window), 1000, 500);
 
+  // label and edits
+  label_edit_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL,0);
+  gtk_container_add (GTK_CONTAINER (window), label_edit_box);
+   
+  int i;
+
+  for (i = 0; i < 2; i++) {
+    edit[i] = gtk_entry_new();
+    if (i == 0)
+      label[i] = gtk_label_new("Login");
+    if (i == 1)
+      label[i] = gtk_label_new("Password");
+    gtk_box_pack_start (GTK_BOX (label_edit_box), label[i], TRUE, 0, 5);
+	  gtk_box_pack_start (GTK_BOX (label_edit_box), edit[i], TRUE, 0, 5);
+	  gtk_widget_show (edit[i]);
+    gtk_widget_show (label[i]);
+  }
+
+  //buttons
   button_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
   gtk_container_add (GTK_CONTAINER (window), button_box);
 
   button_log = gtk_button_new_with_label ("LOG IN");
-  g_signal_connect_swapped (button_log, "clicked", G_CALLBACK (open_chat), window);
-  //g_signal_connect_swapped (button_log, "clicked", G_CALLBACK (gtk_widget_destroy), window);
+  g_signal_connect (GTK_BUTTON(button_log), "clicked", G_CALLBACK (open_chat), window);
   gtk_container_add (GTK_CONTAINER (button_box), button_log);
 
   button_reg = gtk_button_new_with_label ("REGISTER NOW");
-  g_signal_connect (button_reg, "clicked", G_CALLBACK (reg_window), NULL);
-  g_signal_connect_swapped (button_reg, "clicked", G_CALLBACK (gtk_widget_destroy), window);
+  g_signal_connect (GTK_BUTTON(button_reg), "clicked", G_CALLBACK (open_reg), window);
   gtk_container_add (GTK_CONTAINER (button_box), button_reg);
 
+  forg_password = gtk_button_new_with_label ("Forgot password");
+  g_signal_connect (GTK_BUTTON(forg_password), "clicked", G_CALLBACK (change_password), window);
+  gtk_container_add (GTK_CONTAINER (button_box), forg_password);
+
+//css link
+GtkCssProvider *provider = gtk_css_provider_new ();
+ gtk_css_provider_load_from_path (provider, "/Users/vostroverk/Desktop/uchat/Client/src/base/theme.css", NULL);
+
+//css add selector
+GtkStyleContext *context;
+ context = gtk_widget_get_style_context(button_reg);
+ gtk_style_context_add_provider (context, GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_USER);
+ gtk_style_context_add_class(context,"button_reg");
+
+ GtkStyleContext *context2;
+ context2 = gtk_widget_get_style_context(window);
+ gtk_style_context_add_provider (context2, GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_USER);
+ gtk_style_context_add_class(context2,"window");
+
   gtk_widget_show_all (window);
+  //gtk_widget_show (GTK_WIDGET (label_edit_box));
 }
 
 int main (int argc, char **argv)
@@ -49,31 +96,9 @@ int main (int argc, char **argv)
   int status;
   app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
   g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-  status = g_application_run (G_APPLICATION (app), argc, argv);
+  status = g_application_run(G_APPLICATION (app), argc, argv);
   g_object_unref (app);
 
   return status;
 }
 
-/*static void activate (GtkApplication* app, gpointer user_data)
-{
-  GtkWidget *window;
-  window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "Uchat");
-  gtk_window_set_default_size (GTK_WINDOW (window), 1000, 700);
-  gtk_widget_show_all (window);
-  registration();
-}
-
-int main(int argc, char *argv[]) 
-{
-    GtkApplication *app;
-    int status;
-
-    app = gtk_application_new ("uchat", G_APPLICATION_FLAGS_NONE);
-    g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-    status = g_application_run (G_APPLICATION (app), argc, argv);
-    g_object_unref (app);
-
-    return status;
-}*/
