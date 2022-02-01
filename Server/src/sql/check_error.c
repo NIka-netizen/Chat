e620e99a00d490a66858b7aaa97f9da755bf844c
NIka-netizen <58162327+NIka-netizen@users.noreply.github.com>
2022-02-01 21:53:54 +0200

    Delete Server directory

--- Server/src/sql/check_error.c
+++
@@ -1,9 +0,0 @@
-#include "server.h"
-
-int mx_check(int rc, char *err, char *desc) {
-    if (rc != SQLITE_OK) {
-        fprintf(stderr, "SQL error [%s]: %s\n", desc, err);
-        sqlite3_free(err);
-    }
-    return rc;
-}
--- Server/src/sql/close_db.c
+++
@@ -1,5 +0,0 @@
-#include "server.h"
-
-void mx_close_db(sqlite3 *db) {
-    sqlite3_close(db);
-}
