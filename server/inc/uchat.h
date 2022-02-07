3632d0a583d83e6291ee148c47ceb4a1c6c85d8d
NIka-netizen <58162327+NIka-netizen@users.noreply.github.com>
2022-02-07 12:12:54 +0200

    Add files via upload

---
+++ server/inc/uchat.h
@@ -0,0 +1,174 @@
+#ifndef CHAT_H
+#define CHAT_H
+
+
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <locale.h>
+#include <time.h>
+#include <stdint.h>
+#include <math.h>
+#include <unistd.h>
+#include <stdarg.h> 
+
+#include <errno.h>
+#include <fcntl.h>
+// #include <sys/sendfile.h>
+#include <sys/uio.h>
+//ext libs headers
+#include "json-c/json.h"
+#include "mysql/mysql.h"
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+typedef struct
+{
+  int function_number;
+  char **data_array;
+  int counter;
+} data_from_json_t;
+
+typedef struct contact_s
+{
+  char *c_id;
+  char *u_name;
+  char *u_avatar;
+  char *u_lastSeen;
+  char *u_status; //online/offline
+} contact_t;
+
+typedef struct
+{
+  char *u_id;
+  char *u_login;
+  char *u_name;
+  char *u_surname;
+  char *u_avatar;
+  char *u_lastSeen;
+  char *u_secret_word;
+} user_t;
+
+typedef struct message_s
+{
+  char *ch_id;
+  char *ms_id;
+  char *u_id;
+  // char *u_name;
+  char *ms_text;
+  char *ms_datetime;
+  char *ms_isedited;
+  char *ms_isforwarded;
+  char *ms_ismedia;
+  char *ms_isreply;
+  char *ms_isseen;
+  char *ms_media;
+} message_t;
+
+typedef struct chat_s
+{
+  char *ch_id;
+  char *ch_name;
+  char *ch_avatar;
+} chat_t;
+
+typedef struct chatuser_s
+{
+  char *ch_id;
+  char *u_id;
+  char *ch_isadmin;
+} chatuser_t;
+
+//utility
+char *mx_strnew(const int);
+char *itobase10(char *, int);
+char *strjoin(char *, char *);
+char *mx_strcat(char *restrict , const char *restrict );
+char *strjoins_arr(const char **str_arr);
+char *set_date(); // gets curdate
+char *get_filename_extension(char *filename);
+
+//parser
+data_from_json_t parse_json_to_data(char *json_str);
+
+//functions handler
+char *handle_functions(MYSQL *connection, data_from_json_t data, int sock);
+
+//validators
+char *validate_password(MYSQL *connection, char *login, char *password); //log in
+char *is_login_already_taken(MYSQL *connection, char *login); //check is login free
+
+
+//formatting answer_strings
+char *write_message_to_json(message_t message);
+char *contact_to_json(contact_t contact);
+char *write_user_info_to_json(user_t user);
+char *chat_to_json(chat_t chat);
+char *chatuser_to_json(chatuser_t chatuser);
+
+//errors
+void connection_close_error(MYSQL *connection);
+void finish_with_error(MYSQL *con);
+
+//connections
+void *connection_handler(void *socket_descriptor);
+MYSQL *db_connection_open();
+void db_connection_close(MYSQL *connection);
+
+//files
+
+char *recieve_file(MYSQL *connection, int socket, char *number, char *info, char *base_filename);
+char *send_file(int socket, char *fullfilename);
+char *mx_file_to_str(const char *file);
+
+//mysql_requests 
+
+//chats
+char *add_personal_chat(MYSQL *connection, char *my_id, char *contact_id);
+char *edit_chat_name(MYSQL *connection, char *ch_id, char *new_name);
+char *delete_chat(MYSQL *connection, char *ch_id); //also clears chat messages
+char *get_chats(MYSQL *connection, char *u_id);
+char *get_users_from_chat(MYSQL *connection, char *ch_id);
+char *create_group_chat(MYSQL *connection, char *my_id, char *chat_name, char *avatar);
+char *add_user_to_chat(MYSQL *connection, char *ch_id, char *u_id, char *ch_isadmin);
+char *set_chat_avatar(MYSQL *connection, char *ch_id, char *avatar_path);
+
+//user
+char *get_my_id(MYSQL *connection, char *login);
+char *add_user(MYSQL *connection, char *login, char *pass, char *name, char *surname, char *secret_word);
+char *get_user_info(MYSQL *connection, char *u_id);
+char *edit_user_name(MYSQL *connection, char *u_id , char *new_name);
+char *edit_user_surname(MYSQL *connection, char *u_id , char *new_surname);
+char *edit_password(MYSQL *connection, char *u_id, char *new_pass);
+char *set_user_avatar(MYSQL *connection, char *u_id, char *avatar_path);
+
+//contacts
+char *add_contact_by_id(MYSQL *connection,char *contact_id, char *u_id);
+char *add_contact_by_login(MYSQL *connection, char *login, char *u_id);
+char *block_contact(MYSQL *connection, char *contact_id, char *u_id);
+char *unblock_contact(MYSQL *connection, char *contact_id, char *u_id);
+char *delete_contact(MYSQL *connection, char *contact_id, char *u_id);
+char *get_contacts(MYSQL *connection, char *u_id);
+
+//messages
+char *add_message(MYSQL *connection, char *ch_id, char *u_id, char *text, char *is_media, char *isforwarded, char *isreply, char *isseen, char *isedited);
+char *delete_message(MYSQL *connection, char *ms_id);
+char *edit_message(MYSQL *connection, char *ms_id, char *new_text);
+char *forward_message(MYSQL *connection, char *ms_id, char *forward_to_ch_id);
+char *reply_message(MYSQL *connection, char *ms_id, char *reply_to_ch_id);
+char *get_all_messages(MYSQL *connection, char *ch_id, char *last_message_id, int socket);
+char *get_message_by_id(MYSQL *connection, char *ms_id);
+char *get_messages_by_ids(MYSQL *connection, char **ids_mas);
+char *make_message_seen(MYSQL *connection, char *ms_id);
+char *set_message_file(MYSQL *connection, char *ms_id, char *file_path);
+char *get_n_messages(MYSQL *connection, char *ch_id, char *n, int socket);
+
+
+
+#endif
---
+++ server/src/sql_chat_functions.c
@@ -0,0 +1,271 @@
+#include "uchat.h"
+
+char *add_personal_chat(MYSQL *connection, char *my_id, char *contact_id)
+{
+    char *answer = NULL;
+    char *bdrequest = NULL;
+    if(strcmp(my_id,contact_id) == 0)
+    {
+        const char *strings_to_request[] = {"insert into chat (ch_name,ch_avatar) values (\"Saved messages\",\"no_avatar\");", NULL};
+        bdrequest = strjoins_arr(strings_to_request);
+    }else 
+    {
+        const char *strings_to_request[] = {"insert into chat (ch_name,ch_avatar) values ((select u_name from user where u_id = ", contact_id, "),\"no_avatar\");", NULL};
+        bdrequest = strjoins_arr(strings_to_request);
+    }
+    MYSQL_RES *result;
+    MYSQL_ROW row;
+    int num_fields;
+    puts(bdrequest); //Вывод запроса в консоль
+    if (mysql_query(connection, bdrequest))
+    {
+        finish_with_error(connection);
+    }
+
+    char request_for_ch_id[] = "select max(ch_id) from chat limit 1;";
+    puts(request_for_ch_id); //Вывод запроса в консоль
+
+    if (mysql_query(connection, request_for_ch_id))
+    {
+        finish_with_error(connection);
+    }
+
+    result = mysql_store_result(connection);
+
+    if (result == NULL)
+    {
+        finish_with_error(connection);
+    }
+
+    num_fields = mysql_num_fields(result);
+
+    while ((row = mysql_fetch_row(result)))
+    {
+        for (int i = 0; i < num_fields; i++)
+        {
+            answer = row[i];
+        }
+    }
+    char *ch_id = strdup(answer);
+    puts(ch_id);
+    // printf("%d",uid);
+    mysql_free_result(result);
+
+    const char *strings_to_request1[] = {"insert into chatusers (ch_id,u_id,ch_isadmin) values (", ch_id, ",", my_id, ",1);", NULL};
+    bdrequest = strjoins_arr(strings_to_request1);
+    puts(bdrequest); //Вывод запроса в консоль
+    if (mysql_query(connection, bdrequest))
+    {
+        finish_with_error(connection);
+    }
+    if (strcmp(my_id, contact_id) != 0)
+    {
+        const char *strings_to_request2[] = {"insert into chatusers (ch_id,u_id,ch_isadmin) values (", ch_id, ",", contact_id, ",1);", NULL};
+        bdrequest = strjoins_arr(strings_to_request2);
+        if (mysql_query(connection, bdrequest))
+        {
+            finish_with_error(connection);
+        }
+    }
+
+    return "1";
+}
+
+char *create_group_chat(MYSQL *connection, char *my_id, char *chat_name, char *avatar)
+{
+    const char *strings_to_request[] = {"insert into chat (ch_name,ch_avatar) values (\"",chat_name,"\",\"",avatar,"\");", NULL};
+    char *bdrequest = strjoins_arr(strings_to_request);
+    puts(bdrequest); //Вывод запроса в консоль
+    if (mysql_query(connection, bdrequest))
+    {
+        finish_with_error(connection);
+    }
+    const char *strings_to_request1[] = {"SELECT max(ch_id) FROM chat limit 1;", NULL};
+    bdrequest = strjoins_arr(strings_to_request);
+    puts(bdrequest); //Вывод запроса в консоль
+    if (mysql_query(connection, bdrequest))
+    {
+        finish_with_error(connection);
+    }
+    MYSQL_RES *result = mysql_store_result(connection);
+    if (result == NULL)
+    {
+        finish_with_error(connection);
+    }
+    MYSQL_ROW row = mysql_fetch_row(result);
+    puts(row[0]);
+    char *ch_id = strdup(row[0]);
+    add_user_to_chat(connection, ch_id, my_id, "1");
+    return ch_id;
+}
+
+char *edit_chat_name(MYSQL *connection, char *ch_id, char *new_name)
+{
+    const char *request_parts[] = {"UPDATE chat SET ch_name = \"", new_name, "\" WHERE ch_id = \"", ch_id, "\";", NULL};
+    char *bdrequest = strjoins_arr(request_parts);
+
+    puts(bdrequest); //Вывод запроса в консоль
+
+    if (mysql_query(connection, bdrequest))
+    {
+        finish_with_error(connection);
+    }
+    free(bdrequest);
+    char *answer = strdup("1");
+    return answer;
+}
+
+char *delete_chat(MYSQL *connection, char *ch_id)
+{
+    const char *request_parts[] = {"delete * from message where ch_id=\"", ch_id, "\";", NULL};
+    char *bdrequest = strjoins_arr(request_parts);
+    puts(bdrequest); //Вывод запроса в консоль
+    if (mysql_query(connection, bdrequest))
+    {
+        finish_with_error(connection);
+    }
+
+    free(bdrequest);
+    const char *request_parts1[] = {"delete * from chatusers where ch_id=\"", ch_id, "\";", NULL};
+    bdrequest = strjoins_arr(request_parts1);
+
+    puts(bdrequest); //Вывод запроса в консоль
+
+    if (mysql_query(connection, bdrequest))
+    {
+        finish_with_error(connection);
+    }
+
+    free(bdrequest); 
+    const char *request_parts2[] = {"delete * from chat where ch_id=\"", ch_id, "\";", NULL};
+    bdrequest = strjoins_arr(request_parts2);
+
+    puts(bdrequest); //Вывод запроса в консоль
+
+    if (mysql_query(connection, bdrequest))
+    {
+        finish_with_error(connection);
+    }
+    free(bdrequest); 
+    return strdup("1");;
+}
+
+char *get_chats(MYSQL *connection, char *u_id)
+{
+    const char *request_parts[] = {"SELECT c.ch_id,c.ch_name,c.ch_avatar FROM chat c JOIN chatusers cu ON c.ch_id = cu.ch_id WHERE cu.u_id = \"", u_id, "\";", NULL};
+    char *bdrequest = strjoins_arr(request_parts);
+    puts(bdrequest);
+    if (mysql_query(connection, bdrequest))
+    {
+        finish_with_error(connection);
+    }
+    free(bdrequest);
+    MYSQL_RES *result = mysql_store_result(connection);
+    if (result == NULL)
+    {
+        finish_with_error(connection);
+    }
+    int num_rows = mysql_num_rows(result);
+    char **answer = NULL;
+    if (num_rows > 0)
+    {
+        answer = (char **)malloc(sizeof(char *) * num_rows);
+        int i = 0;
+        
+        MYSQL_ROW row;
+        while ((row = mysql_fetch_row(result)))
+        {
+            chat_t chat;
+            chat.ch_id = strdup(row[0]);
+            chat.ch_name = strdup(row[1]);
+            chat.ch_avatar = strdup(row[2]);
+            answer[i] = strdup(chat_to_json(chat));
+            i++;
+        }
+        mysql_free_result(result);
+        char *main_string = strdup("{\"chats\": [");
+        for (i = 0; i < num_rows; i++)
+        {
+            main_string = strjoin(main_string, answer[i]);
+            // strjoin(main_string,contacts[i]);
+            if (i != num_rows - 1)
+                main_string = strjoin(main_string, ",");
+            // strjoin(main_string,",");
+        }
+        main_string = strjoin(main_string, "]}");
+        // strjoin(main_string,"]}");
+        puts(main_string);
+        return main_string;
+    }
+    else
+    {
+        puts("This user have no chats");
+        return strdup("0");
+    }
+}
+
+char *get_users_from_chat(MYSQL *connection, char *ch_id)
+{
+    const char *request_parts[] = {"SELECT * FROM chatusers WHERE ch_id = \"", ch_id, "\";", NULL};
+    char *bdrequest = strjoins_arr(request_parts);
+    puts(bdrequest);
+    if (mysql_query(connection, bdrequest))
+    {
+        finish_with_error(connection);
+    }
+    free(bdrequest);
+    MYSQL_RES *result = mysql_store_result(connection);
+    if (result == NULL)
+    {
+        finish_with_error(connection);
+    }
+    int num_rows = mysql_num_rows(result);
+        char **answer = NULL;
+    if (num_rows > 0)
+    {
+        answer = (char **)malloc(sizeof(char *) * num_rows);
+        int i = 0;
+        
+        MYSQL_ROW row;
+        while ((row = mysql_fetch_row(result)))
+        {
+            chatuser_t chatuser;
+            chatuser.ch_id = strdup(row[0]);
+            chatuser.u_id = strdup(row[1]);
+            chatuser.ch_isadmin = strdup(row[2]);
+            answer[i] = strdup(chatuser_to_json(chatuser));
+            i++;
+        }
+        mysql_free_result(result);
+        char *main_string = strdup("{\"chatusers\": [");
+        for (i = 0; i < num_rows; i++)
+        {
+            main_string = strjoin(main_string, answer[i]);
+            // strjoin(main_string,contacts[i]);
+            if (i != num_rows - 1)
+                main_string = strjoin(main_string, ",");
+            // strjoin(main_string,",");
+        }
+        main_string = strjoin(main_string, "]}");
+        // strjoin(main_string,"]}");
+        puts(main_string);
+        return main_string;
+    }
+    else
+    {
+        puts("This chat have no users ?");
+        return strdup("0");
+    }
+}
+
+char *add_user_to_chat(MYSQL *connection, char *ch_id, char *u_id, char *ch_isadmin)
+{
+    const char *strings_to_request[] = {"insert into chatusers (ch_id,u_id,ch_isadmin) values (\"",ch_id,"\",\"",u_id,"\",\"",ch_isadmin,"\");", NULL};
+    char *bdrequest = strjoins_arr(strings_to_request);
+    puts(bdrequest); //Вывод запроса в консоль
+    if (mysql_query(connection, bdrequest))
+    {
+        finish_with_error(connection);
+    }
+    return strdup("1");
+}
